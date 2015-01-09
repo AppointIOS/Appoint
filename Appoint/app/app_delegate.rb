a7b26ebbf8ed9eb970a991076c74733289f4bf9e
AppointIOS <ath6@macbookair009.local>
2015-01-09 13:57:13 +0100

    Adding new app version

--- Appoint/app/app_delegate.rb
+++ Appoint/app/app_delegate.rb
@@ -1,5 +1,16 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+      
+      # LOADING SCREEN
+      @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+      @window.makeKeyAndVisible
+      
+      
+      # REDIRECT TO AUTH CONTROLLER
+      
+      @window.rootViewController =
+      authController.alloc.initWithNibName(nil, bundle: nil)
+      
     true
   end
 end
---
+++ Appoint/app/authController.rb
@@ -0,0 +1,16 @@
+class authController #< UIViewController
+    def viewDidLoad
+        super
+        
+        puts "Hello mom"
+        
+        #CHECK IF CACHED LOGIN
+        #IF NOT REDIRECT TO LOGIN FORM
+        
+        #USE LOGIN WITH SERVER
+        #IF NOT CORRECT REDIRECT TO LOGIN FORM
+        
+        #IF SUCCESFULL LOGIN REDIRECT TO HOME
+        
+    end
+end
